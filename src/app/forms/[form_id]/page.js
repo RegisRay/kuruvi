a3194c787592d2790596d2c102a312558a50a3ba
Crispin <crispinr003@gmail.com>
2023-04-26 09:13:45 +0530

    fix: add some
    Co-authored-by: Regis Raymond <RegisRay@users.noreply.github.com>
    Co-authored-by: Mouliravi <Mouliravi@users.noreply.github.com>

--- src/app/forms/[form_id]/page.js
+++ src/app/forms/[form_id]/page.js
@@ -1,15 +1,24 @@
 // view a single form
 'use client';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 
 const Form = () => {
+  const [show, setShow] = useState(1);
   return (
     <>
       <section>
         <h3>Trail Suvey / 14-04-2023</h3>
         <div className="d-flex justify-content-center align-items-center">
-          <p>Edit Form</p>
-          <p>Responses</p>
+          <p onclick={setShow(1)}>Edit Form</p>
+          <p onclick={setShow(2)}>Responses</p>
+        </div>
+        <div>
+          {show == 1 ? (
+            <div>this is Form structure page.</div>
+          ) : (
+            <div>this is form responses page.</div>
+          )}
         </div>
       </section>
     </>
