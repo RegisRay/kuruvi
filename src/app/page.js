bbace7daa01b2e96d716a565220da774e4185d42
RegisRay <rregisraymond@gmail.com>
2023-04-27 16:15:28 +0530

    Translate and transcribe done

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,64 @@ import { getAudioTranslation } from './service';
 
 import Auth from 'src/components/Auth';
 import { useAuth, VIEWS } from 'src/components/AuthProvider';
+import { useState } from 'react';
+import { TextWriter } from './writer';
 
 export default function Home() {
   const { initial, user, view, signOut } = useAuth();
+  
+  const [formData, setFormData] = useState(null);
+
+  const handleFile = async (e) => {
+     if (e.target.files && e.target.files[0]) {
+       const file = e.target.files[0];
+      
+       const data = new FormData();
+       data.append("file", file);
+       data.append("model", "whisper-1");
+       setFormData(data);
+ 
+       // check if the size is less than 25MB
+       if (file.size > 25 * 1024 * 1024) {
+         alert("Please upload an audio file less than 25MB");
+         return;
+       }
+     }
+    }
+    
+    const [convertedText, setConvertedText] = useState("");
+    const [translateText, setTranslateText] = useState("");
+    const [loading, setLoading] = useState(false);
+  
+    const sendAudio = async () => {
+      setLoading(true);
+      const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+        headers: {
+          Authorization: `Bearer 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`,
+        },
+        method: "POST",
+        body: formData,
+      });
+  
+      const data = await res.json();
+  
+      setConvertedText(data.text);
+      const res1 =  await fetch("https://api.openai.com/v1/audio/translations", {
+        headers: {
+          Authorization: `Bearer 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`,
+        },
+        method: "POST",
+        body: formData,
+      });
+      const data1 = await res1.json();
+      
+      setTranslateText(data1.text);
+      setLoading(false);
+      
+      
+  
+    };
+   
 
   if (initial) {
     return <div className="card h-72">Loading...</div>;
@@ -35,9 +90,23 @@ export default function Home() {
         <button type="button" className="button-inverse" onClick={signOut}>
           Sign Out
         </button>
+        
+        <input
+          type="file"
+          accept="audio/*"
+          onChange={handleFile}
+        />
+        <button onClick={sendAudio} >
+          Send Audio
+        </button>
+        {loading?(<>Loading</>):(<>
+          <TextWriter text={convertedText} delay={10}/>
+          <TextWriter text={translateText} delay={10}/>
+        </>)}
       </div>
     );
   }
 
   return <Auth view={view} />;
 }
+
